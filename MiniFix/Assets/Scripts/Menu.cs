499da16da5c88aa19068ac783369a4b88b7b6c42
Aron Gilli <arongilli95@gmail.com>
2020-02-01 11:19:00 +0100

    ui in game

---
+++ MiniFix/Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject firstSelectedObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartMenu", 1.5f);
+    }
+
+    public void StartMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        GameObject.Find ("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(firstSelectedObj, null);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    } 
+}
---
+++ MiniFix/Assets/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform PlayerButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void CorrectButton()
+    {
+        PlayerButtons.localPosition -= new Vector3(0, 95, 0);
+    }
+
+    public void WrongButton()
+    {
+        PlayerButtons.localPosition += new Vector3(0, 95, 0);
+    }
+}
