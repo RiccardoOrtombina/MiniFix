e381411a379d0841cd4315bb367ed9605a985bfc
RiccardoOrtombina <48363465+RiccardoOrtombina@users.noreply.github.com>
2020-02-02 13:06:34 +0100

    Animator script

--- MiniFix/Assets/Scripts/AnimationManager.cs
+++ MiniFix/Assets/Scripts/AnimationManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isHammering;
+    public bool isIdle;
+    public bool isRunning;
+
+    Vector3 runDistance = new Vector3(0.1f, 0, 0);
+    public float LeftBound = -10;
+    public float RightBound = 10;
+    bool right = true;
+    bool left;
+
+    Animator animator;
+
+    private void OnEnable()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetBool("isHammering", isHammering);
+        animator.SetBool("isIdle", isIdle);
+        animator.SetBool("isRunning", isRunning);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (isRunning == true)
+        {
+            if(transform.position.x > RightBound)
+            {
+                left = true;
+                right = false;
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            else if(transform.position.x < LeftBound)
+            {
+                left = false;
+                right = true;
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            if(left == true)
+            {
+                transform.Translate(-runDistance);
+            }
+
+            else if(right == true)
+            {
+                transform.Translate(runDistance);
+            }
+            
+        }
     }
+
 }
